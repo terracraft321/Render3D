842946601102632adc6c837802d732f7eee45a8b
Terracraft <6617067+terracraft321@users.noreply.github.com>
2022-09-22 03:08:38 +0300

    v1

--- matrix_functions.py
+++ matrix_functions.py
@@ -1,40 +1,45 @@
-import math 
-import numpy as np 
+import math
+import numpy as np
+
 
 def translate(pos):
-    tx, ty, tz = pos 
-    return np.array([
-        [1, 0, 0, tx],
-        [0, 1, 0, ty],
-        [0, 0, 1, tz],
-        [tx, ty, tz, 1]
-    ])
-    
-def rotate(a):
+    tx, ty, tz = pos
     return np.array([
         [1, 0, 0, 0],
-        [0, math.cos(a), -math.sin(a), 0],
-        [0, math.sin(a), math.cos(a), 0],
+        [0, 1, 0, 0],
+        [0, 0, 1, 0],
+        [tx, ty, tz, 1]
+    ])
+
+
+def rotate_x(a):
+    return np.array([
+        [1, 0, 0, 0],
+        [0, math.cos(a), math.sin(a), 0],
+        [0, -math.sin(a), math.cos(a), 0],
         [0, 0, 0, 1]
     ])
-    
+
+
 def rotate_y(a):
     return np.array([
-        [math.cos(a), 0, math.sin(a), 0],
+        [math.cos(a), 0, -math.sin(a), 0],
         [0, 1, 0, 0],
-        [-math.sin(a), 0, math.cos(a), 0],
+        [math.sin(a), 0, math.cos(a), 0],
         [0, 0, 0, 1]
     ])
-    
+
+
 def rotate_z(a):
     return np.array([
-        [math.cos(a), -math.sin(a), 0, 0],
+        [math.cos(a), math.sin(a), 0, 0],
         [-math.sin(a), math.cos(a), 0, 0],
         [0, 0, 1, 0],
         [0, 0, 0, 1]
     ])
 
-def scale_n(n):
+
+def scale(n):
     return np.array([
         [n, 0, 0, 0],
         [0, n, 0, 0],
--- projection.py
+++ projection.py
@@ -13,7 +13,7 @@ class Projection:
 
         m00 = 2 / (RIGHT - LEFT)
         m11 = 2 / (TOP - BOTTOM)
-        m22 = (FAR + NEAR) / (NEAR - FAR)
+        m22 = (FAR + NEAR) / (FAR - NEAR)
         m32 = -2 * NEAR * FAR / (FAR - NEAR)
         self.projection_matrix = np.array([
             [m00, 0, 0, 0],
@@ -22,9 +22,9 @@ class Projection:
             [0, 0, m32, 0]
         ])
 
-        HW, HH = render.H.WIDTH, render.H_HEIGHT
+        HW, HH = render.H_WIDTH, render.H_HEIGHT
         self.to_screen_matrix = np.array([
-            [HW, 0, 0 0],
+            [HW, 0, 0, 0],
             [0, -HH, 0, 0],
             [0, 0, 1, 0],
             [HW, HH, 0, 1]
